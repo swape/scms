4706e29068ab0c2c7a35e2d0ae8694a3168b8304
Alireza Balouch <gswape@gmail.com>
2025-09-17 17:43:31 +0200

    showing tabs

--- src/components/project/index.svelte
+++ src/components/project/index.svelte
@@ -4,6 +4,7 @@ const ID = window.location.search.split('=')[1]
 import { projects } from '../../store'
 
 let currentProject = $state({})
+let tab = $state('')
 
 projects.subscribe((value) => {
   if (value && value.length > 0) {
@@ -11,12 +12,37 @@ projects.subscribe((value) => {
   }
 })
 
-// TODO: show tabs for project settings and users
+function changeTab(selectedTab) {
+  tab = selectedTab
+}
 </script>
 
 {#if currentProject?.id}
-  {ID}
-  {currentProject.title}
+  <div class="flex border-b border-slate-700 mb-5">
+    <button class={['btn p-3', tab === '' ? 'active' : '']} type="button" onclick={() => changeTab('')}>Summery</button>
+    <button class={['btn p-3', tab === 'settings' ? 'active' : '']} type="button" onclick={() => changeTab('settings')}>Settings</button>
+    <button class={['btn p-3', tab === 'users' ? 'active' : '']} type="button" onclick={() => changeTab('users')}>Users</button>
+  </div>
+  {#if tab === ''}
+    <div>
+      Summery {ID}
+      {currentProject.title}
+    </div>
+  {/if}
+
+  {#if tab === 'settings'}
+    <div>
+      Settings {ID}
+      {currentProject.title}
+    </div>
+  {/if}
+
+  {#if tab === 'users'}
+    <div>
+      Users {ID}
+      {currentProject.title}
+    </div>
+  {/if}
 {/if}
 {#if !currentProject?.id}
   Oops! Project not found.

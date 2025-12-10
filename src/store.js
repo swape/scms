9c80642a025b7dbab1059f8cc55122acffce507a
Alireza Balouch <gswape@gmail.com>
2025-12-10 15:45:34 +0100

    fix: update store to include currentProject and selectedElement

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { writable } from "svelte/store";
 
 export const userObj = writable(null)
-
-export const projects = writable([{ id: '1', title: 'test' }])
+export const currentProject = writable(null)
+export const projects = writable([{ id: '1', title: 'test', pages: {} }])
+export const selectedElement = writable(null)

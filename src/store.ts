910e95f0b20ad18b92ab42a55ecf8051c37b98bb
Alireza Balouch <gswape@gmail.com>
2025-12-15 13:54:09 +0100

    fix: add missing content property to initial projects state in store

--- src/store.ts
+++ src/store.ts
@@ -4,5 +4,5 @@ import { writable } from "svelte/store";
 
 export const userObj = writable(null)
 export const currentProject: Writable<ProjectType | null> = writable(null)
-export const projects: Writable<ProjectType[] | null> = writable([{ id: '1', title: 'test', pages: {} }])
+export const projects: Writable<ProjectType[] | null> = writable([{ id: '1', title: 'test', pages: {}, content: {} }])
 export const selectedElement = writable(null)

db4916efb90dc9b874f1f3aa39af93d6624d0c6a
Alireza Balouch <gswape@gmail.com>
2025-12-15 15:10:01 +0100

    feat: add getBlocksAsArray function to retrieve and sort block elements by pageId

--- src/components/editor/helper.ts
+++ src/components/editor/helper.ts
@@ -1,8 +1,13 @@
 
-import type { PageType} from '../../types/types'
+import type { PageType, ContentListType} from '../../types/types'
 
 
 export function getPagesAsArray(pagesObj:PageType, parentId: null | string = null) {
   const pages = Object.values(pagesObj || {})
   return pages.filter((p) => p?.parent === parentId).toSorted((a, b) => a.order - b.order)
+}
+
+export function getBlocksAsArray(content: ContentListType, pageId: string, parentId: null | string = null) { 
+  const contents = Object.values(content || [])
+  return contents.filter((c) => c?.pageId === pageId && c?.parent === parentId).toSorted((a, b) => a.order - b.order)
 }

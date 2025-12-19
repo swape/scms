47f4579202adb37e4301bf26b8ce670d5f84339f
Alireza Balouch <gswape@gmail.com>
2025-12-19 08:16:06 +0100

    feat: enhance state management by adding subscriptions for selectedPage and currentProject

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,7 @@
 import type { Writable } from 'svelte/store'
 import { writable } from 'svelte/store'
 import type { ProjectType } from './types/types'
+import { getStorage, saveStorage } from './localstorage'
 
 export const userObj = writable(null)
 export const currentProject: Writable<ProjectType | null> = writable(null)
@@ -8,3 +9,27 @@ export const projects: Writable<ProjectType[] | null> = writable([
   { id: '1', title: 'test', pages: {}, content: {} },
 ])
 export const selectedElement = writable(null)
+
+export const selectedPage = writable<string | null>(null)
+
+
+selectedPage.subscribe((value) => {
+  if (value) {
+    saveStorage('selectedPage', value)
+  }
+})
+ 
+currentProject.subscribe((project) => {
+  if (project) {
+    saveStorage('currentProject', project)
+  }
+})
+
+window.addEventListener('storage', (event) => {
+  if (event.key === 'selectedElement') { 
+    const selectedFromStorage = getStorage('selectedElement')
+    if (selectedFromStorage && selectedFromStorage.type !== 'page') {
+      selectedElement.set(selectedFromStorage)
+    }
+  }
+})

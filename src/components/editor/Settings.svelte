083061f598e4362eda803fba28344b691be275c1
Alireza Balouch <gswape@gmail.com>
2026-07-02 20:31:08 +0200

    feat(editor): ✨ enhance Settings component layout and structure

--- src/components/editor/Settings.svelte
+++ src/components/editor/Settings.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
 import { selectedElement } from '../../store.ts'
+import PageElementEdit from './elements/Page/index.svelte'
 
 selectedElement.subscribe((value) => {
   console.log('selectedElement', value)
 })
 </script>
 
-<div class="p-5">
+<div class="p-5 settings">
   {#if $selectedElement}
-    <div>{$selectedElement.type}</div>
-    <div>{$selectedElement.title}</div>
-    <div>{$selectedElement.id}</div>
+    <div class="mb-4 pb-2 border-b border-gray-800">
+      <div><span class="text-gray-500 min-w-15 inline-block">Type:</span> {$selectedElement.type}</div>
+      <div><span class="text-gray-500 min-w-15 inline-block">Title:</span> {$selectedElement.title}</div>
+      <small class="text-gray-500"><span class="min-w-15 inline-block">ID:</span> {$selectedElement.id}</small>
+    </div>
+    {#if $selectedElement.type === 'page'}
+      <PageElementEdit />
+    {/if}
   {/if}
 </div>

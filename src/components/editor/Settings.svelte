20310a9027dbdc1d2d397d08332aa296407c9ab1
Alireza Balouch <gswape@gmail.com>
2026-07-02 20:36:22 +0200

    feat(editor): ✨ improve layout of selected element display in Settings component

--- src/components/editor/Settings.svelte
+++ src/components/editor/Settings.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
 import { selectedElement } from '../../store.ts'
 import PageElementEdit from './elements/Page/index.svelte'
-
-selectedElement.subscribe((value) => {
-  console.log('selectedElement', value)
-})
 </script>
 
 <div class="p-5 settings">
   {#if $selectedElement}
     <div class="mb-4 pb-2 border-b border-gray-800">
-      <div><span class="text-gray-500 min-w-15 inline-block">Type:</span> {$selectedElement.type}</div>
+      <div class="flex gap-2 items-center">
+        <span class="text-gray-500 min-w-15 inline-block">Type:</span> <span class="material-symbols-outlined">{$selectedElement.type === 'page' ? 'draft' : 'brick'}</span>
+        {$selectedElement.type}
+      </div>
       <div><span class="text-gray-500 min-w-15 inline-block">Title:</span> {$selectedElement.title}</div>
       <small class="text-gray-500"><span class="min-w-15 inline-block">ID:</span> {$selectedElement.id}</small>
     </div>

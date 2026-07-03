cd4bc33b3ebfa07076f790e584b6504d8fcf35d0
Alireza Balouch <gswape@gmail.com>
2026-07-03 16:45:55 +0200

    fix(editor): 🐛 correct path initialization and improve layout for selected page display

--- src/components/editor/EditorTopMenu.svelte
+++ src/components/editor/EditorTopMenu.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 import { currentProject, isDarkMode, selectedPage } from '../../store.ts'
 
-let path = $state()
+let path = $state('')
 
 function toggleDarkMode() {
   isDarkMode.update((value) => !value)
@@ -21,35 +21,36 @@ function findParentTitle(pageId: string | null): string {
 }
 
 selectedPage.subscribe((page) => {
-  if (page) {
-    const pagePath = findParentTitle(page.id)
-    path = pagePath
-  } else {
-    path = ''
-  }
+  path = page ? findParentTitle(page.id) : ''
 })
 </script>
 
 <div class="flex gap-2 p-5 border-b border-gray-600 items-center justify-between">
-  <div>
-    {$currentProject?.title || 'No project selected'}
-  </div>
+  <div class="flex gap-2 items-center">
+    <div>
+      {$currentProject?.title || 'No project selected'}
+    </div>
 
-  <div class="overflow-hidden whitespace-nowrap text-ellipsis max-w-[50vw]">
-    {#if $selectedPage}
-      <span class="text-gray-400">{path}</span>
-    {:else}
-      <span class="text-gray-400">No page selected</span>
-    {/if}
+    <div class="overflow-hidden whitespace-nowrap text-ellipsis max-w-[50vw]">
+      {#if $selectedPage}
+        <span class="text-gray-400">{path}</span>
+      {/if}
+    </div>
   </div>
-
-  <button class="flex items-center gap-1 btn hover:bg-slate-600 px-5 py-4" onclick={toggleDarkMode}>
+  <button class="top-menu-button btn" onclick={toggleDarkMode}>
     <span class="material-symbols-outlined">dark_mode</span>
     <span>Toggle Dark Mode</span>
   </button>
 
-  <button class="flex items-center gap-1 btn hover:bg-slate-600 px-5 py-4" onclick={() => alert('Save functionality not implemented yet')}>
-    <span class="material-symbols-outlined">save</span>
-    <span>Save and Publish</span>
-  </button>
+  <div class="flex gap-2">
+    <button class="top-menu-button btn" onclick={() => alert('Save functionality not implemented yet')}>
+      <span class="material-symbols-outlined">save</span>
+      <span>Save</span>
+    </button>
+
+    <button class="top-menu-button btn" onclick={() => alert('Save functionality not implemented yet')}>
+      <span class="material-symbols-outlined">save</span>
+      <span>Save and Publish</span>
+    </button>
+  </div>
 </div>

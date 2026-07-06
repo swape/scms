973226614fa00f7b7def8b26fc31cf675bf443d4
Alireza Balouch <gswape@gmail.com>
2026-07-06 08:28:13 +0200

    feat(settings): ✨ implement debounced content update logic for selected elements

--- src/components/editor/Settings.svelte
+++ src/components/editor/Settings.svelte
@@ -1,7 +1,40 @@
 <script lang="ts">
-import { selectedElement } from '../../store.ts'
+import { currentProject, selectedElement, selectedPage } from '../../store.ts'
+import type { ContentType, PageType } from '../../types/types.ts'
 import PageElementEdit from './elements/Page/index.svelte'
 import TextElementEdit from './elements/Text/index.svelte'
+
+let debounceTimeout: ReturnType<typeof setTimeout>
+
+function updatePageContentWithDebounce() {
+  clearTimeout(debounceTimeout)
+  debounceTimeout = setTimeout(() => {
+    const project = $currentProject
+    const page = $selectedPage
+    const element = $selectedElement
+
+    if (project && page && element) {
+      const currentPageId = page.id
+      const currentElementId = element.id
+      if (currentPageId && currentElementId) {
+        const updatedPages = project.pages.map((p: PageType) => {
+          if (p.id === currentPageId) {
+            const updatedContent = p.content?.map((c: ContentType) => {
+              if (c.id === currentElementId) {
+                return { ...(element as ContentType) }
+              }
+              return c
+            })
+            return { ...p, content: updatedContent }
+          }
+          return p
+        })
+        currentProject.set({ ...project, pages: updatedPages })
+        selectedPage.set({ ...page, content: updatedPages.find((p) => p.id === currentPageId)?.content ?? [] })
+      }
+    }
+  }, 300)
+}
 </script>
 
 <div class="p-5 settings">
@@ -17,7 +50,7 @@ import TextElementEdit from './elements/Text/index.svelte'
     {#if $selectedElement.type === 'page'}
       <PageElementEdit />
     {:else if $selectedElement.type === 'text'}
-      <TextElementEdit />
+      <TextElementEdit {updatePageContentWithDebounce} />
     {/if}
   {/if}
 </div>

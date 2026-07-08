3372813b5ade6a345eef0d904ff89516fb4085c0
Alireza Balouch <gswape@gmail.com>
2026-07-08 13:20:02 +0200

    feat(settings): ✨ add toggle button for adjustable settings panel width

--- src/components/editor/Settings.svelte
+++ src/components/editor/Settings.svelte
@@ -7,6 +7,8 @@ import PageElementEdit from './elements/Page/index.svelte'
 import TextElementEdit from './elements/Text/index.svelte'
 import Styling from './Styling.svelte'
 
+let toggleFloat = $state(false)
+
 // Updating a single key value for the selected element.
 function update(key: string, value: unknown) {
   selectedElement.update((element) => {
@@ -52,8 +54,12 @@ function updatePageContentWithDebounce() {
 }
 </script>
 
-<div class="p-5 settings">
+<div class={`p-5 settings bg-slate-900 ${toggleFloat ? 'float' : ''}`}>
   {#if $selectedElement}
+    <div>
+      <button title="Toggle width" class="btn flex p-2 rounded-full mb-4" onclick={() => (toggleFloat = !toggleFloat)}
+        ><span class="material-symbols-outlined">{!toggleFloat ? 'chevron_left' : 'chevron_right'}</span></button>
+    </div>
     <div class="mb-4 pb-2 border-b border-gray-800">
       <div class="flex gap-2 items-center">
         <span class="text-gray-500 min-w-15 inline-block">Type:</span> <span class="material-symbols-outlined">{$selectedElement.type === 'page' ? 'draft' : 'brick'}</span>
@@ -74,3 +80,14 @@ function updatePageContentWithDebounce() {
     <Styling {updatePageContentWithDebounce} {update}></Styling>
   {/if}
 </div>
+
+<style>
+.float {
+  position: fixed;
+  right: 0;
+  width: 60%;
+  bottom: 0;
+  top: 150px;
+  overflow-y: auto;
+}
+</style>

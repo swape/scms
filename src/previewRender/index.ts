09e5928e089a3f24788f2ab30c03cac53ffb48e9
Alireza Balouch <gswape@gmail.com>
2026-07-08 14:52:42 +0200

    feat(preview): ✨ implement rendering logic for content and storage event handling

---
+++ src/previewRender/index.ts
@@ -0,0 +1,84 @@
+// @ts-nocheck
+import { getStorage, saveStorage } from '../localstorage.ts'
+import type { ContentType } from '../types/types.ts'
+import { renderHeader } from './headerView.ts'
+
+function renderAll(content: ContentType[]) {
+  const renderTo = document.querySelector('main')
+
+  if (!renderTo || !content || content.length === 0) {
+    return
+  }
+  const output: string[] = []
+  console.log({ content, renderTo })
+  for (const element of content) {
+    if (element.type === 'header') {
+      output.push(renderHeader(element))
+      continue
+    }
+  }
+  renderTo.innerHTML = output.join('\n')
+}
+
+let selectedPage = getStorage('selectedPage')
+let darkMode = getStorage('darkMode') ? true : false
+
+// listen to storage changes and update the page accordingly
+window.addEventListener('storage', (event) => {
+  if (event.key === 'selectedElement') {
+    const selectedFromStorage = JSON.parse(event.newValue)
+    const selectedElementTextarea = document.getElementById('selectedElement')
+    selectedElementTextarea.value = JSON.stringify(selectedFromStorage?.data, null, 2)
+    markSelectedElement(selectedFromStorage?.data?.id, selectedFromStorage?.data?.type)
+  }
+  if (event.key === 'selectedPage') {
+    selectedPage = getStorage('selectedPage')
+    const selectedPageTextarea = document.getElementById('selectedPage')
+    selectedPageTextarea.value = JSON.stringify(selectedPage, null, 2)
+
+    renderAll(selectedPage?.content ?? [])
+  }
+  if (event.key === 'darkMode') {
+    darkMode = getStorage('darkMode') ? true : false
+
+    const darkModeTextarea = document.getElementById('darkMode')
+    darkModeTextarea.value = JSON.stringify(darkMode, null, 2)
+  }
+})
+
+window.addEventListener('click', (event) => {
+  event.preventDefault()
+  // find the closest element with data-type attribute
+  const target = event.target as HTMLElement
+  const closestElement = target.closest('[data-type]') as HTMLElement
+
+  if (!closestElement) {
+    return
+  }
+
+  const type = closestElement.getAttribute('data-type')
+  const id = closestElement.getAttribute('data-id')
+
+  if (!type || !id) {
+    return
+  }
+
+  if (selectedPage?.content?.length > 0) {
+    const selectedElement = selectedPage.content.find((element) => element.id === id && element.type === type)
+    saveStorage('selectedElement', selectedElement)
+  }
+
+  markSelectedElement(id, type)
+})
+
+function markSelectedElement(selectedID: string, selectedType: string) {
+  const allSelectedElements = document.querySelectorAll('[data-selected="true"]')
+  for (const element of allSelectedElements) {
+    element.removeAttribute('data-selected')
+  }
+
+  const elementInDOM = document.querySelector(`[data-type="${selectedType}"][data-id="${selectedID}"]`)
+  if (elementInDOM) {
+    elementInDOM.setAttribute('data-selected', 'true')
+  }
+}

acaad78bef0cf366dfb32f789a3e99b862e5f217
Alireza Balouch <gswape@gmail.com>
2026-07-09 12:46:37 +0200

    feat(editor): ✨ update element display to show type as fallback for title

--- src/components/editor/PageElements.svelte
+++ src/components/editor/PageElements.svelte
@@ -23,7 +23,7 @@ function deleteElementWithConfirmation(elementId: string | number) {
         <button type="button" onclick={() => selectedElement.set(element)} class="flex-1 bg-slate-800 p-2 rounded cursor-pointer hover:bg-slate-700">
           <div class="flex gap-2 items-center" data-id={element.id} data-type={element.type} data-order={element.order}>
             <span class="material-symbols-outlined">{'brick'}</span>
-            <span>{element.title}</span>
+            <span>{element.title || element.type}</span>
           </div>
         </button>
         <button type="button" onclick={() => deleteElementWithConfirmation(element.id)} class=" bg-red-800 p-2 rounded cursor-pointer hover:bg-red-700">
